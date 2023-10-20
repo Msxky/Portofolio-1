80b99a3b9c575f44cc329861598a5314ae792a34
M Sukri <142651234+Msxky@users.noreply.github.com>
2023-10-20 16:56:50 +0700

    Add files via upload

---
+++ assets/js/darkmode.js
@@ -0,0 +1,17 @@
+const toggle = document.querySelector(".darkMode");
+const siang = document.querySelector(".bxs-sun");
+const malam = document.querySelector(".bxs-moon");
+
+siang.style.display = "none"
+
+toggle.addEventListener("click", function () {
+  document.body.classList.toggle("white-theme");
+  
+  if (document.body.classList.contains("white-theme")) {
+    siang.style.display = "block";
+    malam.style.display = "none";
+  } else {
+    malam.style.display = "block";
+    siang.style.display = "none";
+  }
+});
