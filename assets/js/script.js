385c686ebefe82d4b57e7d25f22f15540303cb33
msxky <sukri7406@gmail.com>
2024-02-28 05:15:00 +0700

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,51 @@ function trigger() {
     btn.style.border = " 2px solid var(--warna-utama)";
   }
 }
+
+// cursor
+const cursor = document.querySelector(".cursor");
+const cursor2 = document.querySelector(".cursor2");
+const cursor3 = document.querySelector(".cursor3");
+var timeout;
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+
+  cursor.style.display = "block";
+  cursor2.style.display = "block";
+  cursor3.style.display = "block";
+
+  cursor2.animate(
+    {
+      left: `${x}px`,
+      top: `${y}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+
+  cursor3.animate(
+    {
+      left: `${x}px`,
+      top: `${y}px`,
+    },
+    { duration: 2000, fill: "forwards" }
+  );
+
+  document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+    cursor2.style.display = "none";
+    cursor3.style.display = "none";
+  });
+
+  function mouseStop() {
+    cursor.style.display = "none";
+    cursor2.style.display = "none";
+    cursor3.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStop, 2000);
+});
